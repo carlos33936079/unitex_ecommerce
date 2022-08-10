4aec2a791a66b39bb9ee4a7bbc3ce4acd00bb057
charly <c.l.08.cl@gmail.com>
2022-08-10 10:02:49 -0300

    1

---
+++ app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './ui/layout/App';
+
+import "./assets/styles/styles.scss"
+
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
+
+
---
+++ app/src/ui/layout/App.js
@@ -0,0 +1,19 @@
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer'
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Main/>
+      <Footer/>
+      <ToastContainer />
+    </BrowserRouter>
+    )
+}
+
+export default App;
