cc1a2fe5d92c844c44b43bc4ea07528a42346c2b
charly <c.l.08.cl@gmail.com>
2022-09-06 17:56:15 -0300

    cambios 6/9/22

---
+++ app/src/ui/layout/components/main/home/Exclusive.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import CardProductContainer from "../productCard/CardProductContainer"
+
+function Exclusive({productos, loading}) {
+    console.log(productos)
+  return (
+        <div className='homeMain_products_first'>
+            { loading ? "Cargando Productos..." : productos.map((producto, i)=>{
+            return producto.habilitado ? <CardProductContainer key= {i} {...producto}/> : null })}
+                
+
+        </div>
+  )
+}
+
+export default Exclusive
--- app/src/ui/layout/components/main/home/HomeMain.js
+++ app/src/ui/layout/components/main/home/HomeMain.js
@@ -1,17 +1,49 @@
 import React from 'react'
-import CardProductContainer from "../productCard/CardProductContainer"
+import Exclusive from './Exclusive'
+import {useState, useEffect} from "react"
+import { toast } from 'react-toastify'
+import {useLocation, useParams} from 'react-router-dom'
+import { db } from '../../hook/Firebase'
+import { getDocs, collection, query, where } from 'firebase/firestore'
+
 function HomeMain() {
+
+  const [loading, setLoading] = useState(true)
+  const [productos, setProductos] = useState([])
+
+  useEffect(()=>{
+      setLoading(true)
+      const productCollection = collection(db,"productos")
+      let documentProduct = []
+      documentProduct = getDocs(productCollection)
+      documentProduct
+                  .then(respuesta=>{
+                    const aux = []
+                    respuesta.forEach(element => {
+                      const product = {
+                        id: element.id,
+                        ...element.data()
+                      }
+                      aux.push(product)
+                    })
+                    setProductos(aux)
+                  })         
+      .catch((error404)=>{
+        toast.error('Error al cargar el Producto')
+      })
+      .finally((fin)=>{
+        setLoading(false)   
+      })
+    
+},[])
+
+// console.log(productos)
+
   return (
     <div className='homeMain_container'>
         <h1 className='homeMain_title'>- PRODUCTOS EXCLUSIVOS -</h1>
         <div className='homeMain_products'>
-            <div className='homeMain_products_first'>
-                <CardProductContainer/>
-                <CardProductContainer/>
-                <CardProductContainer/>
-                <CardProductContainer/>
-
-            </div>
+        {productos ? <Exclusive productos={productos} loading={loading}/>: <div>No se encontraron productos...</div>}
             <div className='homeMain_products_menu'>
                 <li>TODO LO QUE BUSCAS</li>
                 <li>TELAS POR NOMBRE</li>
